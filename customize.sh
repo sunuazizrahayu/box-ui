47e9c18cda1eb010d1551305f64118d16f613ebf
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2024-11-25 20:23:39 +0700

    fix: install module when update

--- customize.sh
+++ customize.sh
@@ -6,26 +6,28 @@ php_data_dir="/data/adb/php7"
 php_bin_dir="${php_data_dir}/files/bin"
 
 ui_print "********************************"
-ui_print "*        PHP7 Webserver        *"
-ui_print "*         by: nosignal         *"
-ui_print "********************************"
-ui_print ""
-
-ui_print "Installing PHP7 Webserver..."
-ui_print "[0] Creating directory..."
+ui_print "[>] Creating directory..."
 mkdir -p ${MODPATH}/system/bin
 mkdir -p ${php_data_dir}
 
+temp_bak=$(mktemp -d "${php_data_dir}/php7.XXXXXXXXXX")
+temp_dir="${temp_bak}"
 
+ui_print "[>] Extracting module"
 unzip -o "${ZIPFILE}" -x 'META-INF/*' -d $MODPATH >&2
 
-ui_print "[20] Copying files..."
+if [ -d ${php_data_dir} ]; then
+	mv ${php_data_dir}/* "${temp_dir}/"
 
+	ui_print "[>] Remove old version"
+	rm -rf ${temp_dir}
+fi
+
+ui_print "[>] Installing Box UI"
 mv ${MODPATH}/files ${php_data_dir}
 mv ${MODPATH}/scripts ${php_data_dir}
 
-ui_print "[90] Setting Files Permission..."
-
+ui_print "[>] Setting files permission"
 set_perm_recursive ${MODPATH} 0 0 0755 0644
 set_perm_recursive ${php_data_dir} 0 0 0755 0644
 set_perm_recursive ${php_data_dir}/scripts 0 0 0755 0755
@@ -43,9 +45,9 @@ set_perm  ${php_data_dir}/files/config/php.config ${system_uid} ${system_gid} 07
 set_perm  ${php_data_dir}/files/config/php.ini ${system_uid} ${system_gid} 0755
 # chmod +x ${php_data_dir}/files
 
-ui_print "[100] Done..."
-ui_print " "
-ui_print "-     Config Files      : ${php_data_dir}/files/config"
-ui_print "-     Default htdocs    : ${php_data_dir}/files/www"
-ui_print "-     Web Services      : http://127.0.0.1:80"
-ui_print "-     TTYD Services     : http://127.0.0.1:3001"
+ui_print "[>] FINISH - Please reboot your device."
+ui_print "********************************"
+ui_print "- Config Files      : ${php_data_dir}/files/config"
+ui_print "- Default htdocs    : ${php_data_dir}/files/www"
+ui_print "- Web Services      : http://127.0.0.1:80"
+ui_print "- TTYD Services     : http://127.0.0.1:3001"
