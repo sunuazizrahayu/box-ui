e815afc26eba512daddbaa7e8f9f07d782e1222e
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2024-11-28 17:30:54 +0700

    add blade like

--- files/www/layout/Template.php
+++ files/www/layout/Template.php
@@ -1,26 +1,27 @@
 <?php
+
 class Template {
 
-    static $blocks = [];
+    static $blocks = []; // Menyimpan blok section
     static $cache_path = 'cache/';
     static $cache_enabled = false;
     static $extension = '.blade.php';
 
-    // Main method for rendering views
+    // Fungsi untuk menampilkan view
     static function view($file, $data = []) {
         $cached_file = self::cache($file);
         extract($data, EXTR_SKIP);
         require $cached_file;
     }
 
-    // Caching the compiled version of the view
+    // Fungsi untuk mem-cache view
     static function cache($file) {
         if (!file_exists(self::$cache_path)) {
             mkdir(self::$cache_path, 0744);
         }
         $cached_file = self::$cache_path . str_replace(['/', '.html'], ['_', ''], $file . '.php');
         
-        // Check if the cache is enabled and the file needs to be recompiled
+        // Jika caching tidak aktif atau file perlu di-recompile
         if (!self::$cache_enabled || !file_exists($cached_file) || filemtime($cached_file) < filemtime($file)) {
             $code = self::includeFiles($file);
             $code = self::compileCode($code);
@@ -30,24 +31,25 @@ class Template {
         return $cached_file;
     }
 
-    // Clear the compiled cache files
+    // Fungsi untuk menghapus cache
     static function clearCache() {
         foreach (glob(self::$cache_path . '*') as $file) {
             unlink($file);
         }
     }
 
-    // Compile the view code (blocks, yields, echo statements, etc.)
+    // Fungsi untuk meng-compile kode
     static function compileCode($code) {
-        $code = self::compileBlock($code);
-        $code = self::compileYield($code);
+        $code = self::compileSection($code);  // Compile single-line @section
+        $code = self::compileBlock($code);    // Compile @section blocks
+        $code = self::compileYield($code);    // Compile @yield statements
         $code = self::compileEscapedEchos($code);
         $code = self::compileEchos($code);
         $code = self::compilePHP($code);
         return $code;
     }
 
-    // Handling includes and extends in Blade-like syntax
+    // Fungsi untuk menangani @include dan @extends
     static function includeFiles($file) {
         $file = $file . self::$extension;
         $code = file_get_contents($file);
@@ -58,12 +60,11 @@ class Template {
             $code = str_replace($match[0], self::includeFiles($match[1]), $code);
         }
 
-        // Remove the includes/extends after processing
         $code = preg_replace('/@(?:extends|include)\([\'"]?(.*?)?[\'"]?\)/i', '', $code);
         return $code;
     }
 
-    // Convert PHP code
+    // Compile PHP code
     static function compilePHP($code) {
         return preg_replace('~@php\s*(.+?)\s*@endphp~is', '<?php $1 ?>', $code);
     }
@@ -78,39 +79,39 @@ class Template {
         return preg_replace('~\{{{\s*(.+?)\s*\}}}~is', '<?php echo htmlentities($1, ENT_QUOTES, \'UTF-8\') ?>', $code);
     }
 
-    // Handle block definitions (@section)
+    // Compile @section blocks
     static function compileBlock($code) {
         preg_match_all('/@section\((.*?)\)(.*?)@endsection/is', $code, $matches, PREG_SET_ORDER);
         
         foreach ($matches as $match) {
             $blockName = $match[1];
             $blockContent = $match[2];
-            self::$blocks[$blockName] = $blockContent;
-            $code = str_replace($match[0], '', $code);
+            self::$blocks[$blockName] = $blockContent; // Store block content
+            $code = str_replace($match[0], '', $code); // Remove the section
         }
-
+        
         return $code;
     }
 
-    // Handle yielding content (@yield)
+    // Compile @yield statements
     static function compileYield($code) {
         foreach (self::$blocks as $blockName => $blockContent) {
-            $code = preg_replace('/@yield\(' . preg_quote($blockName) . '\)/', $blockContent, $code);
+            $code = preg_replace('/@yield\([\'"]?' . preg_quote($blockName, '/') . '[\'"]?\)/', $blockContent, $code);
         }
         
-        // Remove any other @yield that doesn't match a block
-        $code = preg_replace('/@yield\([\'"]?(.*?)?[\'"]?\)/', '', $code);
-        
+        $code = preg_replace('/@yield\([\'"]?(.*?)?[\'"]?\)/', '', $code); // Remove any other @yield
         return $code;
     }
 
-    // Handle parent content (@parent)
-    static function compileParent($code) {
-        return preg_replace_callback('/@parent/', function () {
-            return self::$blocks['parent'] ?? '';
+    // Handle single-line sections like @section('name', 'value')
+    static function compileSection($code) {
+        // Match and process single-line @section('name', 'value')
+        $code = preg_replace_callback('/@section\([\'"]?(.*?)?[\'"]?,\s*([\'"]?)(.*?)\2\s*\)/', function($matches) {
+            self::$blocks[$matches[1]] = $matches[3]; // Store the section content in the blocks array
+            return '';  // Remove the section from the code
         }, $code);
+
+        return $code;
     }
-
 }
-
 ?>
