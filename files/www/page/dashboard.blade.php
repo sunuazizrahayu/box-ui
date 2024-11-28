9765b4d72d3d02d0b1077ad2ee6b9027358243bd
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2024-11-28 23:20:33 +0700

    update dashboard

--- files/www/page/dashboard.blade.php
+++ files/www/page/dashboard.blade.php
@@ -9,171 +9,126 @@
 <!-- Main content -->
 <section class="content">
 
-	<!-- System -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">System</h3>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="nav-tabs-custom">
+				<ul class="nav nav-tabs">
+					<li class="active"><a href="#tab_1" data-toggle="tab">System</a></li>
+					<li><a href="#tab_2" data-toggle="tab">Memory</a></li>
+					<li><a href="#tab_3" data-toggle="tab">Storage</a></li>
+					<li><a href="#tab_4" data-toggle="tab">Port Status</a></li>
+					<li><a href="#tab_5" data-toggle="tab">Network</a></li>
+					<li><a href="#tab_6" data-toggle="tab">Active DHCP Leases</a></li>
+				</ul>
+				<div class="tab-content no-padding">
+					<div class="tab-pane active" id="tab_1">
+						<table class="table">
+							<tr>
+								<td>Model</td>
+								<td>{{ $device_model }}</td>
+							</tr>
+							<tr>
+								<td>Firmware Version</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Kernel Version</td>
+								<td>{{ $kernel_version }}</td>
+							</tr>
+							<tr>
+								<td>Local Time</td>
+								<td>{{ $time_now }}</td>
+							</tr>
+							<tr>
+								<td>Uptime</td>
+								<td><?=$uptime ?></td>
+							</tr>
+							<tr>
+								<td>Temperature</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td>CPU Usage</td>
+								<td>{{ number_format($cpu_usage, 2) }}%</td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab_2">
+						<table class="table">
+							<tr>
+								<td>RAM Total</td>
+								<td>{{ $memory['total_memory_text'] }}</td>
+							</tr>
+							<tr>
+								<td>Used</td>
+								<td>{{ $memory['total_used_memory_text'] }}</td>
+							</tr>
+							<tr>
+								<td>Buffered</td>
+								<td>{{ $memory['buffered_text'] }}</td>
+							</tr>
+							<tr>
+								<td>Cached</td>
+								<td>{{ $memory['cached_text'] }}</td>
+							</tr>
+							<tr>
+								<td>Free</td>
+								<td>{{ $memory['free_text'] }}</td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab_3">
+						<table class="table">
+							<tr>
+								<td>Disk space</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Temp space</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>/dev/root</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab_4">
+						<table class="table">
+							<tr>
+								<td>XXXX</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab_5">
+						<table class="table">
+							<tr>
+								<td>XXXX</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab_6">
+						<table class="table">
+							<tr>
+								<td>Disk space</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Temp space</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>/dev/root</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>Model</td>
-					<td>{{ $device_model }}</td>
-				</tr>
-				<tr>
-					<td>Firmware Version</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Kernel Version</td>
-					<td>{{ $kernel_version }}</td>
-				</tr>
-				<tr>
-					<td>Local Time</td>
-					<td>{{ $time_now }}</td>
-				</tr>
-				<tr>
-					<td>Uptime</td>
-					<td><?=$uptime ?></td>
-				</tr>
-				<tr>
-					<td>Temperature</td>
-					<td>-</td>
-				</tr>
-				<tr>
-					<td>CPU Usage</td>
-					<td>{{ number_format($cpu_usage, 2) }}%</td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
 	</div>
-	<!-- /.box -->
-
-	<!-- Memory / RAM -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Memory</h3>
-		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>RAM Total</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Used</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Buffered</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Cached</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Uptime</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Temperature</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>CPU Usage</td>
-					<td></td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
-	</div>
-	<!-- /.box -->
-
-	<!-- Storage -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Storage</h3>
-		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>Disk space</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Temp space</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>/dev/root</td>
-					<td></td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
-	</div>
-	<!-- /.box -->
-
-	<!-- Storage -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Port status</h3>
-		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>XXXX</td>
-					<td></td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
-	</div>
-	<!-- /.box -->
-
-	<!-- Network -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Network</h3>
-		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>XXXX</td>
-					<td></td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
-	</div>
-	<!-- /.box -->
-
-	<!-- Active DHCP Leases -->
-	<div class="box">
-		<div class="box-header with-border">
-			<h3 class="box-title">Active DHCP Leases</h3>
-		</div>
-		<div class="box-body no-padding">
-			<table class="table">
-				<tr>
-					<td>Disk space</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Temp space</td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>/dev/root</td>
-					<td></td>
-				</tr>
-			</table>
-		</div>
-		<!-- /.box-body -->
-	</div>
-	<!-- /.box -->
 
 </section>
 <!-- /.content -->
--- files/www/page/dashboard2.php
+++ files/www/page/dashboard2.php
@@ -73,11 +73,72 @@ function getTime($format='')
 	$current_date = date($format);
 	return $current_date;
 }
+function getMemory()
+{
+	$total_memory_kb = shell_exec('cat /proc/meminfo | grep MemTotal | awk \'{print $2}\'');
+	$total_memory_gb = intval(trim($total_memory_kb)) / 1024 / 1024; // Convert to GB
+	$total_memory_gb_rounded = number_format($total_memory_gb, 2);
+	$total_memory_mb_rounded = number_format($total_memory_gb * 1024, 2);
+
+	$free_memory_kb = shell_exec('cat /proc/meminfo | grep MemFree | awk \'{print $2}\'');
+	$free_memory_gb = intval(trim($free_memory_kb)) / 1024 / 1024; // Convert to GB
+	$free_memory_gb_rounded = number_format($free_memory_gb, 2);
+	$free_memory_mb_rounded = number_format($free_memory_gb * 1024, 2);
+
+	$buffers_memory_kb = shell_exec('cat /proc/meminfo | grep Buffers | awk \'{print $2}\'');
+	$buffers_memory_gb = intval(trim($buffers_memory_kb)) / 1024 / 1024; // Convert to GB
+	$buffers_memory_gb_rounded = number_format($buffers_memory_gb, 2);
+	$buffers_memory_mb_rounded = number_format($buffers_memory_gb * 1024, 2);
+	$buffers_text = $buffers_memory_mb_rounded . ' MB';
+	if ($buffers_memory_gb >= 1) {
+		$buffers_text = $buffers_memory_gb_rounded . ' GB';
+	}
+
+	$cached_memory_kb = shell_exec('cat /proc/meminfo | grep ^Cached | awk \'{print $2}\'');
+	$cached_memory_gb = intval(trim($cached_memory_kb)) / 1024 / 1024; // Convert to GB
+	$cached_memory_gb_rounded = number_format($cached_memory_gb, 2);
+	$cached_memory_mb_rounded = number_format($cached_memory_gb * 1024, 2);
+	$cached_text = $cached_memory_mb_rounded . ' MB';
+	if ($cached_memory_gb >= 1) {
+		$cached_text = $cached_memory_gb_rounded . ' GB';
+	}
+
+	$used_memory_gb = floatval($total_memory_gb_rounded) - floatval($free_memory_gb_rounded) - floatval($buffers_memory_gb_rounded) - floatval($cached_memory_gb_rounded);
+	$used_memory_mb = floatval($total_memory_mb_rounded) - floatval($free_memory_mb_rounded) - floatval($buffers_memory_mb_rounded) - floatval($cached_memory_mb_rounded);
+	$used_memory_percent = number_format((($used_memory_gb / $total_memory_gb_rounded) * 100),2);
+
+	$available_memory_gb = floatval($free_memory_gb_rounded) + floatval($buffers_memory_gb_rounded) + floatval($cached_memory_gb_rounded);
+	$available_memory_mb = floatval($free_memory_mb_rounded) + floatval($buffers_memory_mb_rounded) + floatval($cached_memory_mb_rounded);
+
+
+	$total_memory_text = $total_memory_mb_rounded.' MB';
+	if ($total_memory_gb_rounded >= 1) {
+		$total_memory_text = $total_memory_gb_rounded.' GB';
+	}
+	$total_used_memory_text = $used_memory_mb.' MB';
+	if ($used_memory_gb >= 1) {
+		$total_used_memory_text = $used_memory_gb.' GB';
+	}
+	$available_memory_text = $available_memory_mb.' MB';
+	if ($available_memory_gb >= 1) {
+		$available_memory_text = $available_memory_gb.' GB';
+	}
+
+	return [
+		'total_memory_text' => $total_memory_text,
+		'total_used_memory_text' => $total_used_memory_text,
+		'total_available_memory_text' => $available_memory_text,
+		'buffered_text' => $buffers_text,
+		'cached_text' => $cached_text,
+		'free_text' => $available_memory_text,
+	];
+}
 
 $data['device_model'] = getDeviceModel();
 $data['kernel_version'] = getKernelVersion();
 $data['uptime'] = getUptime('%dd %hh %mm %ss');
 $data['cpu_usage'] = getCpuUsage();
 $data['time_now'] = getTime('Y-m-d H:i:s');
+$data['memory'] = getMemory();
 $data['page_title'] = 'Dashboard';
 view('page/dashboard', $data);
