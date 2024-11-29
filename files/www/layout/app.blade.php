4ad1b06e02da6876c65ea7798923620d26b1a56c
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2024-11-29 09:04:51 +0700

    update icon

--- files/www/layout/app.blade.php
+++ files/www/layout/app.blade.php
@@ -9,6 +9,7 @@ $app_url = site_url();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>@yield('page_title')</title>
+  <link rel="icon" type="image/x-icon" href="/webui/assets/img/icon-trim.png">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -124,10 +125,9 @@ $app_url = site_url();
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.18
+      <b>Version</b> 1.0.3-rc
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
-    reserved.
+    <strong>BOX-UI &copy; <?=date('Y') ?> by <a href="https://github.com/sunuazizrahayu/box-ui">kangsunu</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
